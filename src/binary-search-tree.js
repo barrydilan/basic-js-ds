abe6a702a474acba842e6096f9a4cba6aaef1dcc
barrydillan <queharambe@gmail.com>
2023-04-17 19:48:39 +0200

    done

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,11 +1,13 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
 class BinarySearchTree {
 
   root() {
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let node = new ListNode;
+  node.next = l;
+  let current = node;
+  
+  while (current) {
+    if (current.value === k) {
+      if (prev) {
+        prev.next = current.next;
+      } else {
+        this.head = current.next;
+      }
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+  return node.next;
 }
 
 module.exports = {
